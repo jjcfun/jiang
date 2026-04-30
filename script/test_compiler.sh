10b0db1ca729ac84c9bd7adf80d9d514a9dccd8b
JiangJunChen <junchen@jiang.fun>
2026-04-30 18:51:57 +0800

    feat(backend): add llvm codegen facade

--- script/test_compiler.sh
+++ script/test_compiler.sh
@@ -92,6 +92,7 @@ run_all_compiler_samples() {
   run_compiler_sample type_minimal.jiang 0
   run_compiler_sample lower_hir_minimal.jiang 0
   run_compiler_sample lower_jir_minimal.jiang 0
+  run_compiler_sample codegen_llvm_minimal.jiang 0
   run_compiler_compile_fail invalid_array_list_set_immutable_type_arg.jiang
 }
 
