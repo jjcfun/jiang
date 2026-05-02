98462d70a954902ef11f67cd7e88c391964dcdad
JiangJunChen <junchen@jiang.fun>
2026-05-02 13:36:56 +0800

    fix(compiler): preserve mutable globals and logical short-circuit

--- script/stage1_smoke.sh
+++ script/stage1_smoke.sh
@@ -76,6 +76,7 @@ run_sample minimal.jiang 42
 run_sample locals_minimal.jiang 42
 run_sample assign_minimal.jiang 5
 run_sample binary_ops_minimal.jiang 1
+run_sample logical_ops_minimal.jiang 140
 run_sample if_expr_minimal.jiang 42
 run_sample struct_minimal.jiang 42
 run_sample fields_minimal.jiang 3
