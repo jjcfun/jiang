395f7272bd60652fb44d1d69becc12533132eb94
JiangJunChen <junchen@jiang.fun>
2026-06-10 21:22:23 +0800

    feat(driver): use package objects for release

--- script/backend_cli_smoke.sh
+++ script/backend_cli_smoke.sh
@@ -19,6 +19,10 @@ sample_obj="$SMOKE_BUILD_DIR/minimal.o"
 sample_from_ll="$SMOKE_BUILD_DIR/minimal_from_ll"
 sample_from_obj="$SMOKE_BUILD_DIR/minimal_from_obj"
 sample_with_link_arg="$SMOKE_BUILD_DIR/minimal_with_link_arg"
+sample_release_obj="$SMOKE_BUILD_DIR/minimal_release.o"
+sample_from_release_obj="$SMOKE_BUILD_DIR/minimal_from_release_obj"
+sample_release_bin="$SMOKE_BUILD_DIR/minimal_release"
+package_release_bin="$SMOKE_BUILD_DIR/package_release"
 field_sample="$SMOKE_BUILD_DIR/field_projection.jiang"
 field_ll="$SMOKE_BUILD_DIR/field_projection.ll"
 field_bin="$SMOKE_BUILD_DIR/field_projection"
@@ -50,6 +54,24 @@ test -s "$sample_obj"
 
 "$compiler_bin" --link-arg -Wl,-dead_strip -o "$sample_with_link_arg" "$sample"
 "$sample_with_link_arg"
+
+"$compiler_bin" --mode release --emit-obj -o "$sample_release_obj" "$sample"
+test -s "$sample_release_obj"
+"$clang_bin" "$sample_release_obj" -o "$sample_from_release_obj" \
+  $("$LLVM_CONFIG" --ldflags) \
+  $("$LLVM_CONFIG" --libs all) \
+  $("$LLVM_CONFIG" --system-libs)
+"$sample_from_release_obj"
+
+"$compiler_bin" --mode release -o "$sample_release_bin" "$sample"
+"$sample_release_bin"
+
+"$compiler_bin" --mode release -o "$package_release_bin" test/lang/package/run/source_dependency_app
+set +e
+"$package_release_bin"
+package_status=$?
+set -e
+test "$package_status" -eq 52
 
 "$compiler_bin" --emit-llvm -o "$field_ll" "$field_sample"
 test -s "$field_ll"
