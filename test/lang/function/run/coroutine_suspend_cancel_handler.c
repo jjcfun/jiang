b0620a77f117e92404b69f711cc16dc6f7782927
JiangJunChen <junchen@jiang.fun>
2026-07-14 22:57:45 +0800

    feat(async): add cancellable coroutine continuations

---
+++ test/lang/function/run/coroutine_suspend_cancel_handler.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <sched.h>
+#include <stdatomic.h>
+#include <stdint.h>
+#include <unistd.h>
+
+static _Atomic int64_t registration_ready;
+static _Atomic int64_t registration_open;
+static pthread_t opener_thread;
+
+void suspend_cancel_registration_block(void) {
+    atomic_store(&registration_ready, 1);
+    while (atomic_load(&registration_open) == 0) {
+        sched_yield();
+    }
+}
+
+void suspend_cancel_registration_wait_ready(void) {
+    while (atomic_load(&registration_ready) == 0) {
+        sched_yield();
+    }
+}
+
+static void *open_registration(void *opaque) {
+    (void)opaque;
+    usleep(10000);
+    atomic_store(&registration_open, 1);
+    return 0;
+}
+
+void suspend_cancel_registration_open_later(void) {
+    pthread_create(&opener_thread, 0, open_registration, 0);
+}
+
+void suspend_cancel_registration_join(void) {
+    pthread_join(opener_thread, 0);
+}
